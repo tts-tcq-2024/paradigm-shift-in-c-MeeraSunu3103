d2866ed5f2b68ef57e05234a68651c3c0408b2b5
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-08-27 11:14:55 +0530

    Update checker.c
    created array first to display only the parameter that was breached first ; modified printResult function accordingly

--- checker.c
+++ checker.c
@@ -2,16 +2,28 @@
 #include <assert.h>
 
 char parameter[3][25] = {"Temperature","State of Charge","Charge rate"};
-int high[3] = {45,80,0.8};
-int low[3] = {0,20,0};
+float high[3] = {45,80,0.8};
+float low[3] = {0,20,0};
+int first[3] = {0,0,0};
+int i;
 
-void printResult(int index) {
-  printf("%s is out of range!\n", parameter[index]);
+void printResult() {
+  int ix;
+  for(i = 0; i < 3; ++i)
+  {
+      if(first[i] == 1) {
+          ix = i;
+          break;
+      } else {
+          continue;
+      }
+  }
+  printf("%s is out of range!\n", parameter[ix]);
 }
 
 int parameterIsOk(float value, int index) {
-  if(value < low[index] || value > high[index]) {
-    printResult(index);
+  if((value < low[index]) || (value > high[index])) {
+    first[index]++;
     return 0;
   } else {
     return 1;
@@ -22,6 +34,11 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
   if(((parameterIsOk(temperature, 0)) + (parameterIsOk(soc, 1)) + (parameterIsOk(chargeRate, 2))) == 3) {
     return 1;
   } else {
+    printResult();
+    for(i = 0; i < 3; ++i)
+    {
+      first[i] = 0;
+    }
     return 0;
   }
 }

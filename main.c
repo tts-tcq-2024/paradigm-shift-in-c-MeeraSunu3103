23716ebb560bd2f1098430f2c9d83bf5fd113cef
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-09 15:19:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main() {
     /* Change tolerance values as required for tuning */
+    /* If any of the battery condition parameters does not have a tolerance specified, set it to 0 */
     const float toleranceTemperature = 5;
     const float toleranceStateOfCharge = 5;
     const float toleranceChargeRate = 5;
@@ -10,7 +11,7 @@ int main() {
     float testvaluesStateOfCharge[5];
     float testvaluesChargeRate[5];
 
-    /* creates test input values based on lower limit, upper limit and given tolerance */
+    /* creates test input values based on lower limit, upper limit and given tolerance of the battery condition parameters */
     setTestValues(toleranceTemperature, toleranceStateOfCharge, toleranceChargeRate,
                   testvaluesTemperature, testvaluesStateOfCharge, testvaluesChargeRate);
 
